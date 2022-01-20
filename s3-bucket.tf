7b030078811c4a86eb170f0a2595f3e9b29ba613
Manish Warang <manish6385@gmail.com>
2022-01-20 06:06:41 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/hashicorp-g33kzone-demo/s3-bucket/aws"
-  version = "2.13.0"
+  version = "2.2.0"
   # insert required variables here
   bucket_prefix = "g33kzone"
 }
